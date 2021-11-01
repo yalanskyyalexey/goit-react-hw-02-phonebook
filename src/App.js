fa1114c0c1b7130204cea9bf4626cf4689ce9c06
yalanskyyalexey <yalanskyyalexey@gmail.com>
2021-11-01 12:00:58 +0200

    добавил husky

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 function App() {
-	return (
-		<div className='App'>
-			<h1>Hello</h1>
-		</div>
-	);
+  return (
+    <div className="appWrapper">
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
